8047c4536a4655a0707fc593d66b8f6de46e81ea
Jatin Gupta <guptajatin@outlook.com>
2020-07-08 18:30:18 +1000

    rmeove redundant lines

--- submission.py
+++ submission.py
@@ -17,8 +17,6 @@ def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
     def p(hash, query, offset, alpha):
         if count_collisions(hash[1], query, offset) >= alpha:
              return hash[0]
-        # else:
-        #     return -1
 
     offset = 0
     found = False
@@ -26,22 +24,8 @@ def c2lsh(data_hashes, query_hashes, alpha_m, beta_n):
     while not found:
         rdd = data_hashes.map(lambda hash: p(hash, query_hashes, offset, alpha_m)).filter(lambda x: x is not None)
 
-        res = rdd.collect()
-        s = set(res)
-        if len(s) >= beta_n:
+        if rdd.count() >= beta_n:
             found = True
         else:
-            print(s)
             offset += 1
     return rdd
-
-    # while not found:
-        # for each data D in data hashes:
-            # if count (D, query_hashes, offset) > alpha_m:
-                # cand <- cand.union(i)
-        # if count(cand) < beta_n:
-            #offset += 1
-        # else:
-        #   found = True
-        # pass
-    #return cand
